0219946f8690057c33a9df6bac943a78e015daa3
ysminskmd <ys.minsk.md@yandex.ru>
2019-02-04 17:10:47 +0300

    relative_layout

--- app/src/main/java/com/mds2019/MainActivity.java
+++ app/src/main/java/com/mds2019/MainActivity.java
@@ -2,12 +2,32 @@ package com.mds2019;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner mAges;
+    private TextView mName;
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative_layout);
+
+        mName = findViewById(R.id.name);
+        mSpinner = findViewById(R.id.cities);
+        mAges = findViewById(R.id.ages);
+    }
+
+    public void clickButton(View view) {
+        final String age = String.valueOf(mAges.getSelectedItem());
+        final String city = String.valueOf(mSpinner.getSelectedItem());
+        final String name = String.valueOf(mName.getText());
+
+        Toast.makeText(this, "Name = " + name + ", city = " + city + ", age = " + age, Toast.LENGTH_SHORT).show();
     }
 }
